78c21f8d35ea6d299f71b530e89c646ef3d9be15
mihainicolaescu <mihaita_nicolaescu@yahoo.com>
2023-09-26 00:20:10 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import { useState } from "react";
+import "./Style/App.css";
+
+
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(1);
+  const [message, setMessage] = useState("Today is");
+  var date = new Date().toLocaleDateString("de-DE");
+
+
+  function modifyCount(value){
+    setCount((curentCount) => curentCount + (step * value));
+    var msg = "";
+    if(count === 0){
+      msg = "Today is"
+    }else if (count >= 1){
+      msg = count + " day ago it was ";
+    } else {
+      msg = count * -1 + " days ago will be ";
+    }
+    
+    setMessage(msg);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="menu">
+        <button onClick={() => {setStep((c) => c - 1)}}>-</button>
+        <p>Step: {step}</p>
+        <button onClick={() => {setStep((c) => c + 1)}}>+</button>
+      </div>
+      <div className="menu">
+      <button onClick={()=> {modifyCount(-1)}}>-</button>
+        <p>Count: {count}</p>
+        <button  onClick={()=> {modifyCount(1)}}>+</button>
+      </div>
+      <div className="menu">
+        <p className="Message">{message} {date}</p>
+      </div>
     </div>
   );
 }
